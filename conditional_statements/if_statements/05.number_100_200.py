a9ccd904897a54964c73e820e370f40aad96f194
saliko.programista <155167057+saliokka@users.noreply.github.com>
2024-01-22 16:08:05 +0200

    Update 05.number_100_200.py

--- conditional_statements/if_statements/05.number_100_200.py
+++ conditional_statements/if_statements/05.number_100_200.py
@@ -1,9 +1,9 @@
-num = int(input())
+number = int(input())
 
-if num < 100:
+if number < 100:
     print("Less than 100")
 
-elif num <= 200:
+elif number <= 200:
     print("Between 100 and 200")
 
 else:
